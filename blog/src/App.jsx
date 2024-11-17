937e8d9330b8e44d02f36d782a9a6d67a0dfde34
ohwooje <dkwooje@naver.com>
2024-11-17 17:37:00 +0900

    UseRef input의 ref

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,25 +1,26 @@
-import { useState, useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
+
 
 function App() {
-  const [count,setCount] = useState(1);
-  const renderCount = useRef(0);
+
+  const inputRef = useRef();
+
+  useEffect(()=>{
+    console.log(inputRef);
+    inputRef.current.focus();
+
+}, []);
 
 
- 
-  useEffect(() => {
-    renderCount.current = renderCount.current + 1;
-    console.log('렌더링 수:',renderCount.current);
-  })
-
-  // '올려'버튼으로 랜더링이 되고 useState가 작동되어 컴포넌트 함수가 다시 불러 useEffect가 업데이트가 되는데
-  // useEffect가 업데이트 되면 랜더링이 시작되기 때문에 다시 useState가 작동되어
-  // 결과적으로 무한 루프에 빠진다.
- 
+  function login(){
+    alert(`환영합니다. ${inputRef.current.value}!`);
+    inputRef.current.focus();
+  }
 
   return (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={()=>setCount(count +1)}>올려</button>
+      <input ref={inputRef} type="text" placeholder="username" />
+      <button onClick={login}>로그인</button>
     </div>
   );
 }
