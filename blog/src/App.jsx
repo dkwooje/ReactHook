635709c5786b0cef0e7932fe0fd57b86b261ff25
ohwooje <dkwooje@naver.com>
2024-11-17 15:59:23 +0900

    UseState 함수형2

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,21 +1,40 @@
 import { useState } from "react";
 
+function heaveyWork(){
+  console.log('무거운 작업');
+  return(['홍길동', '김민수']);
+}
+
 function App() {
 
-  const [time,setTime] = useState(1);
+  // const [names,setNames] = useState(heaveyWork());
+  //위의 코드를 사용하면 랜더링할 때 마다 사용되므로 비효율적이다.
 
-  function handleTime(){
-    if(time>=12){
-      setTime(1);
-    }else{
-      setTime(time + 1);
-    }
-  } 
+  const [names,setNames] = useState(
+    function(){                   //익명함수: 한 번만 사용하거나 특정한 곳에서 바로 실행되도록 정의
+    return heaveyWork();     
+  });
+
+  const [input,setInput] = useState('');
+
+  function handleInput(e){
+    setInput(e.target.value);
+  }
+
+  function handleUpload(){
+    setNames((prevState) => {
+      console.log('이전 state', prevState);
+      return([input, ...prevState])
+    });
+  }
 
   return (
     <div>
-      <span>현제 시각: {time}시</span>
-      <button onClick={handleTime}>Update</button>
+      <input type="text" value={input} onChange={handleInput} />
+      <button onClick={handleUpload}>upload</button>
+      {names.map(function(name, idx){
+        return (<p key={idx}>{name}</p>)
+      })}
     </div>
   );
 }
