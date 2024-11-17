702bbf0270f1d5c60772a6f45ce05f51bac683a2
ohwooje <dkwooje@naver.com>
2024-11-17 16:56:06 +0900

    UseRef 기본

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
-import Timer from "./component/Timer";
+import { useState, useRef } from "react";
 
 function App() {
 
-  const [showTimer,setShowTimer] = useState(false);
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+
+  console.log(countRef);
+  console.log('렌더링');
+
+  function increaseState(){
+    setCount(count + 1);
+  }
+
+  function increaseRef(){
+    countRef.current = countRef.current + 1;
+    console.log('Ref: ', countRef.current);
+  }
 
   return (
     <div>
-      {showTimer && <Timer />}
-      <button onClick={function(){
-        setShowTimer(!showTimer)
-      }}
-        >Toggle Timer</button>
+      <p>State: {count}</p>
+      <p>Ref: {countRef.current}</p>
+      <button onClick={increaseState}>State 올려</button>
+      <button onClick={increaseRef}>Ref 올려</button>
     </div>
   );
 }
