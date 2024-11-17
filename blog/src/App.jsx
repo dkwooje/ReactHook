7aea436f79881245ad8a2ad07693b32a71f0aff3
ohwooje <dkwooje@naver.com>
2024-11-17 16:31:20 +0900

    UseEffect 종속성

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -12,11 +12,26 @@ function handleCount(){
 function handleInput(e){
   setName(e.target.value)
 }
-
-//랜더링 될때마다 매번 실행된
+/*
+//랜더링 될때마다 매번 실행된다.(종속성 배제)
   useEffect(() => {
     console.log('랜더링');
-  })
+  });
+
+  //종속성 추가
+  useEffect(() => {
+    console.log('count 변화');
+  },[count]);
+
+  useEffect(() => {
+    console.log('name 변화');
+  },[name]);
+  */
+
+  useEffect(() => {
+    console.log('마운팅');
+  },[]);
+
 
   return (
     <div>
