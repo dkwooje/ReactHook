f90036916b6742b005c622cb23136a521eab9cd2
ohwooje <dkwooje@naver.com>
2024-11-17 17:25:55 +0900

    UseRef 오류 해결

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState, useRef, useEffect } from "react";
 
 function App() {
-  const [count,setCount] = useState(0);
-  const [renderCount, setRenderCount] = useState(1);
- /*
+  const [count,setCount] = useState(1);
+  const renderCount = useRef(0);
+
+
+ 
   useEffect(() => {
-    console.log('렌더링');
-    setRenderCount(renderCount + 1);
+    renderCount.current = renderCount.current + 1;
+    console.log('렌더링 수:',renderCount.current);
   })
-    */
+
+  // '올려'버튼으로 랜더링이 되고 useState가 작동되어 컴포넌트 함수가 다시 불러 useEffect가 업데이트가 되는데
+  // useEffect가 업데이트 되면 랜더링이 시작되기 때문에 다시 useState가 작동되어
+  // 결과적으로 무한 루프에 빠진다.
+ 
 
   return (
     <div>
