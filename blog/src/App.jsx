a185d55132fdd97acc0908d227d5c814be983b82
ohwooje <dkwooje@naver.com>
2024-11-17 16:42:43 +0900

    UseEffect 타이머

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,44 +1,14 @@
 import { useEffect, useState } from "react";
+import Timer from "./component/Timer";
 
 function App() {
 
-  const [count,setCount] = useState(1);
-  const [name,setName] = useState('');
-
-function handleCount(){
-  setCount(count + 1);
-}
-
-function handleInput(e){
-  setName(e.target.value)
-}
-/*
-//랜더링 될때마다 매번 실행된다.(종속성 배제)
-  useEffect(() => {
-    console.log('랜더링');
-  });
-
-  //종속성 추가
-  useEffect(() => {
-    console.log('count 변화');
-  },[count]);
-
-  useEffect(() => {
-    console.log('name 변화');
-  },[name]);
-  */
-
-  useEffect(() => {
-    console.log('마운팅');
-  },[]);
-
+  const [showTimer,setShowTimer] = useState(false);
 
   return (
     <div>
-      <button onClick={handleCount}>Update</button>
-      <span>count: {count}</span>
-      <input type="text" value={name} onChange={handleInput} />
-      <span>name: {name}</span>
+      {showTimer && <Timer />}
+      <button onClick={()=> setShowTimer(!showTimer)}>Toggle Timer</button>
     </div>
   );
 }
