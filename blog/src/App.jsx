e358c0ba45adb78237f0b1c5e2d7ad9ab5f4ad45
ohwooje <dkwooje@naver.com>
2024-11-17 16:25:12 +0900

    UseEffect 기본

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,41 +1,29 @@
-import { useState } from "react";
-
-function heaveyWork(){
-  console.log('무거운 작업');
-  return(['홍길동', '김민수']);
-}
+import { useEffect, useState } from "react";
 
 function App() {
 
-  // const [names,setNames] = useState(heaveyWork());
-  //위의 코드를 사용하면 랜더링할 때 마다 사용되므로 비효율적이다.
+  const [count,setCount] = useState(1);
+  const [name,setName] = useState('');
 
-  const [names,setNames] = useState(
-    function(){                   //익명함수: 한 번만 사용하거나 특정한 곳에서 바로 실행되도록 정의
-    return heaveyWork();          //익명함수는 화살표 함수로 바꿔 쓸 수 있다.
-  });
+function handleCount(){
+  setCount(count + 1);
+}
 
-  const [input,setInput] = useState('');
+function handleInput(e){
+  setName(e.target.value)
+}
 
-  function handleInput(e){
-    setInput(e.target.value);
-  }
-
-  function handleUpload(){
-    setNames(
-      function(prevState){
-      console.log('이전 state', prevState);
-      return([input, ...prevState])
-    });
-  }
+//랜더링 될때마다 매번 실행된
+  useEffect(() => {
+    console.log('랜더링');
+  })
 
   return (
     <div>
-      <input type="text" value={input} onChange={handleInput} />
-      <button onClick={handleUpload}>upload</button>
-      {names.map(function(name, idx){
-        return (<p key={idx}>{name}</p>)
-      })}
+      <button onClick={handleCount}>Update</button>
+      <span>count: {count}</span>
+      <input type="text" value={name} onChange={handleInput} />
+      <span>name: {name}</span>
     </div>
   );
 }
