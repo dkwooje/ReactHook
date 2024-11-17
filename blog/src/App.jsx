c8c5cf63fd611fd88fc985a9005ee29dd9883c4b
ohwooje <dkwooje@naver.com>
2024-11-17 15:34:07 +0900

    UseState 기본 연습

---
+++ blog/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function App() {
+
+  const [time,setTime] = useState(1);
+
+  const handleTime = () => {
+    if(time>=12){
+      setTime(1);
+    }else{
+      setTime(time + 1);
+    }
+  } 
+
+  return (
+    <div>
+      <span>현제 시각: {time}시</span>
+      <button onClick={handleTime}>Update</button>
+    </div>
+  );
+}
+
+export default App;
