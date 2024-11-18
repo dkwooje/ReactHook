f4d9376367daa0678301bcde2435e6a8425d1f03
ohwooje <dkwooje@naver.com>
2024-11-18 18:23:29 +0900

    useReducer 기초

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,29 +1,53 @@
-import { useCallback, useState } from "react";
-import Box from "./Box";
+import { useReducer, useState } from "react";
+
+
+const ACTION_TYPE = {
+    deposit: '예금합니다.',
+    withdraw: '출금합니다.'
+}
+
+function reducer(state,action){
+  console.log("reducer가 일을 합니다.", state, action)
+  switch (action.type){
+    case ACTION_TYPE.deposit:
+      return state + action.payload;
+    case ACTION_TYPE.withdraw: 
+      return state - action.payload;
+    default:
+      return state;
+  }
+}
 
 function App() {
 
-  const [size,setSize] = useState(100);
-  const [isDark, setIsDark] = useState(false);
-
-  const BoxStyle = useCallback(()=>{
-    return{
-      backgroundColor: 'yellow',
-      width: `${size}px`,
-      height: `${size}px`
-    };
-  }, [size]);
+  const [number, setNumber] = useState(0);
+  const [money,  dispatch] = useReducer(reducer, 0);
 
   return (
-    <div    style={{background: isDark ? 'black' : 'white'}}>
-      <input type="number" 
-      value={size} 
-      onChange={(e)=> setSize(e.target.value)}
+    <div>
+      <h2>useReducer 은행에 오신것을 환영합니다.</h2>
+      <p>잔고: {money}원</p>
+      <input 
+      type="number"    
+      value={number}     
+      onChange={(e) => setNumber(parseInt(e.target.value))}
+      step="1000"
       />
-      <button onClick={()=>setIsDark(!isDark)}>Change Theme</button>
-    <Box createBoxStyle={BoxStyle} />
+      <button onClick={()=>
+        dispatch({type: ACTION_TYPE.deposit, payload: number})
+      }>예금</button>
+      <button onClick={()=>
+        dispatch({type: ACTION_TYPE.withdraw, payload: number})
+      }>출금</button>
     </div>
   );
 }
 
 export default App;
+
+// useReducer 핵심 : Reducer, Dispatch, Action
+// Reducer: State를 업데이트하는 역활
+// Dispatch: State를 업데이트를 요구하는 역활
+// Action: State 업데이트 내용을 정의하는 역활
+// State를 Dispatch가 Action으로 Reducer한다.
+// Dispatch(Action) -> Reducer(State,Action)
