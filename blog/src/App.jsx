ea1dfd74cca55e231c2a66115f5a8e1ff1fd5882
ohwooje <dkwooje@naver.com>
2024-11-18 16:31:19 +0900

    useMemo 적용후

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,4 +1,4 @@
-import {  useState } from "react";
+import {  useState, useMemo } from "react";
 
 function hardCalculate(number){
   console.log('어려운 계산');
@@ -19,7 +19,12 @@ function App() {
   const [hardNumber, setHardNumber] = useState(1);
   const [easyNumber, setEasyNumber] = useState(1);
 
-  const hardSum = hardCalculate(hardNumber);
+ // const hardSum = hardCalculate(hardNumber);
+ const hardSum = useMemo(() => {
+  return hardCalculate(hardNumber)}, [hardNumber]);
+//useMemo의 첫번째 인자는 메모와이즈할 값을 return, 두번째 인자는 의존성 배열을 넣어준다.
+//의존성 배열의 값이 바뀔 때 만  메모와이즈가 발동한다.
+
   const easySum = easyCalculate(easyNumber);
 
   return (
@@ -43,3 +48,6 @@ function App() {
 
 export default App;
 
+//처음 랜더링 할때는 두 계산기가 불려 오래걸린다.
+//하지만 어려운 계산기는 메모화 해주었기 때문에,
+// 쉬운계산기를 작동시켜 랜더링 할 때 마다. 쉬운 계산기는 빠르게 동작할 수 있다.
