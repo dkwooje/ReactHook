91246fb36358668cb04cc1f593524359044d18e9
ohwooje <dkwooje@naver.com>
2024-11-18 17:33:14 +0900

    useCallback 적용 후

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,13 +1,20 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
 
   const [number,setNumber] = useState(0);
+  const [toggle,setToggle] = useState(true);
 
+/*
   function someFunction(){
     console.log(`someFunc: number: ${number}`);
     return;
   }
+*/
+const someFunction = useCallback(()=>{
+  console.log(`someFunc: number: ${number}`);
+  return;
+},[number])
 
   useEffect(() =>{
     console.log("someFunction이 변경되었습니다.");
@@ -20,6 +27,7 @@ function App() {
       value={number} 
       onChange={(e)=> setNumber(e.target.value)}
       />
+      <button onClick={()=>setToggle(!toggle)}>{toggle.toString()}</button>
       <br />
       <button onClick={someFunction}>Call someFunc</button>
     </div>
@@ -28,6 +36,6 @@ function App() {
 
 export default App;
 
-//useCallback은 두 인자를 받는다. 첫번째는 메모화 할 함수, 두번째는 의존성 배열
-//useEffec를 [someFunction]을 의존성 배열로 설정해주어도 input안의 숫자를 변경하면
-//랜더링되어 App컴포넌트가 초기화실행 되므로 [someFunction]의 의존성이 무시된다.
+//useCallback으로 함수를 메모화 하고, 메모화된 함수의 주소는 someFunction가 가지게 된다.
+//첫번째 인자에 메모화(주소화)할 함수, 두번째 인자는 의존성 배열.
+//두번째 인자가 바뀌면 첫번째 인자도 초기화 되어 someFunction에 새로운 함수 주소가 들어가게 된다.
