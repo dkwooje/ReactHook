7c24ac522cbc5d9bba7794e9b7dd73d8b6ade65f
ohwooje <dkwooje@naver.com>
2024-11-19 18:40:12 +0900

    Custom Hooks 사용후1

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,29 +1,22 @@
-import { useState } from "react";
+import useInput from "./UseInput";
 
 
 function App() {
-   
-  const [inputValue, setInputValue] = useState('');
 
-  function handleChange(e){
-    setInputValue(e.target.value)
-  } 
+  const [inputValue,handleChange] = useInput('안녕');
+  const [inputValue2,handleChange2] = useInput('하세요');
 
   function handleSubmit(){
     alert(inputValue);
-    setInputValue('');
+   // setInputValue(''); //초기화 기능
   }
 
   return (
     <div>
       <h1>useInput</h1>
-      <input 
-      value={inputValue}
-      onChange={handleChange}
-      />
+      <input value={inputValue} onChange={handleChange} />
+      <input value={inputValue2} onChange={handleChange2} />
       <button onClick={handleSubmit}>확인</button>
-
-
     </div>
   );
 }
