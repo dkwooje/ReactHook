fa21eae317e9fdbdbb53fbdf1404073a8dd8e236
ohwooje <dkwooje@naver.com>
2024-11-19 19:30:24 +0900

    useId 기초 3

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -8,8 +8,11 @@ function MyInput(){
 
   return(
     <div>
-      <label htmlFor={id}>이름</label>
-      <input id={id}/>
+      <label htmlFor={`${id} -name`}>이름</label>
+      <input id={`${id} -name`}/>
+      <br />
+      <label htmlFor={`${id} -age`}>나이</label>
+      <input id={`${id} -age`}/>
     </div>
   )
 }
@@ -21,7 +24,7 @@ function App() {
   return (
     <div>
       <MyInput />
-      <MyInput />
+
     </div>
   );
 }
@@ -32,4 +35,4 @@ function App() {
 export default App;
 //useId() input같은 form요소에 접근할 때 유용하다.
 //위와 같이 input과 label을 연동하면 UI에서 label을 눌러도 input에 포커싱할 수 있다.  
-//동일한 컴포넌트가 만들어지만 useId를 활용하여 각각 개별적으로 포커싱할 수 있다.
+//동일한 form요소를 사용할 때 
