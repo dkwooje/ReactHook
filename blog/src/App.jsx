691d7221fd0380f4183bcc4f88789c3d61847725
ohwooje <dkwooje@naver.com>
2024-11-19 19:27:58 +0900

    useId 기초 2

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,19 +1,35 @@
 import { useId } from "react";
 
+
+
+function MyInput(){
+
+  const id = useId();
+
+  return(
+    <div>
+      <label htmlFor={id}>이름</label>
+      <input id={id}/>
+    </div>
+  )
+}
+
 function App() {
 
-  const  id = useId();
-  console.log(id);
+
 
   return (
     <div>
-      <label htmlFor="name">이름</label>
-      <p></p>
-      <input id="name"/>
+      <MyInput />
+      <MyInput />
     </div>
   );
 }
 
+
+
+
 export default App;
 //useId() input같은 form요소에 접근할 때 유용하다.
 //위와 같이 input과 label을 연동하면 UI에서 label을 눌러도 input에 포커싱할 수 있다.  
+//동일한 컴포넌트가 만들어지만 useId를 활용하여 각각 개별적으로 포커싱할 수 있다.
