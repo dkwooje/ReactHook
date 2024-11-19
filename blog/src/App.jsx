4135c700d56d31daba2fba3722548f3a2d149da6
ohwooje <dkwooje@naver.com>
2024-11-19 19:03:03 +0900

    Custom Hooks useEffect fetch 커스텀 훅 적용 전

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,20 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const baseUrl = "https://jsonplaceholder.typicode.com";
 
 
 function App() {
 
-  useEffect(() =>{
-    fetch("https://jsonplaceholder.typicode.com/posts") //users 대신 posts , todos 기입
+  const [data, setData] = useState(null);
+
+  function fetchUrl(type){
+    fetch(baseUrl + '/' + type) 
     .then((res) => res.json())
-    .then((res) => console.log(res));
+    .then((res) => setData(res));
+  }
+
+  useEffect(() =>{
+    fetchUrl("users")//users 대신 posts , todos 기입
   },[]);
+
+  console.log(data);
 
   return (
     <div>
       <h1>useFetch</h1>
+      <button onClick={()=> fetchUrl("users")}>Users</button>
+      <button onClick={()=> fetchUrl("posts")}>Posts</button>
+      <button onClick={()=> fetchUrl("todos")}>Todos</button>
+      <pre>{JSON.stringify(data,null,2)}</pre>
     </div>
   );
 }
 
 export default App;
 //모크데이터 가짜 데이터를 불러보자
+//fetch를 통해 데이터를 받아보자
