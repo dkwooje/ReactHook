dd48b86c54fb082769a6e9ccb6ae9fd81e2e1f5e
ohwooje <dkwooje@naver.com>
2024-11-19 18:27:24 +0900

    Custom Hooks 사용전

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,40 +1,33 @@
-import { useCallback, useState } from "react";
-import Child from "./Child";
+import { useState } from "react";
+
 
 function App() {
-    
-    const [parentAge, setParentAge] = useState(0);
-    
-    function incrementParentAge(){
-      setParentAge(parentAge + 1);
-    }
+   
+  const [inputValue, setInputValue] = useState('');
 
-    console.log('부모 컴포넌트가 렌더링이 되었습니다.');
-/*
-    function tellMe(){
-      console.log('엘리아스 지도자');
-    }
-*/
-  const tellMe = useCallback(()=>{
-    console.log('엘리아스 지도자');
-  },[]);
+  function handleChange(e){
+    setInputValue(e.target.value)
+  } 
+
+  function handleSubmit(){
+    alert(inputValue);
+    setInputValue('');
+  }
 
   return (
-    <div style={
-      {
-        border: '2px solid navy', 
-        padding: '10px'
-      }
-    }>
-      <h1>부모</h1>
-      <p>age: {parentAge}</p>
-      <button onClick={incrementParentAge}>부모 나이 증가</button>
-      <Child name={"에르핀"} tellMe={tellMe} />
+    <div>
+      <h1>useInput</h1>
+      <input 
+      value={inputValue}
+      onChange={handleChange}
+      />
+      <button onClick={handleSubmit}>확인</button>
+
+
     </div>
   );
 }
 
 export default App;
 
-//function tellMe와 같은 함수도 객체변수이므로 주소값이 전달하게 된다.
-//
+//커스텀 훅으로 재사용성을 극대화 할 수 있다.
