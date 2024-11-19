8a1e920283b3f26fa6880ede4f401566f8cc9606
ohwooje <dkwooje@naver.com>
2024-11-19 19:16:40 +0900

    Custom Hooks useEffect fetch 커스텀 훅 적용후 2

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -6,18 +6,19 @@ const baseUrl = "https://jsonplaceholder.typicode.com";
 
 function App() {
 
-  const {data,fetchUrl} = useFetch(baseUrl,"users");
+  const {data: userData} = useFetch(baseUrl,"users");
+  const {data: postData} = useFetch(baseUrl,"users");
 
 
   return (
     <div>
-      <h1>useFetch</h1>
-      <button onClick={()=> fetchUrl("users")}>Users</button>
-      <button onClick={()=> fetchUrl("posts")}>Posts</button>
-      <button onClick={()=> fetchUrl("todos")}>Todos</button>
-      <pre>{JSON.stringify(data,null,2)}</pre>
+      <h1>user</h1>
+      {userData && <pre>{JSON.stringify(userData[1],null,2)}</pre>}
+      <h1>post</h1>
+      {postData && <pre>{JSON.stringify(userData[1],null,2)}</pre>}
     </div>
   );
 }
 
 export default App;
+//
