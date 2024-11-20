b08e170d811609f4e629f29e50339af04531c64c
ohwooje <dkwooje@naver.com>
2024-11-20 15:56:15 +0900

    Debounce 적용전 1

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,54 +1,36 @@
-import { useEffect, useRef, useState, useLayoutEffect } from "react";
-
-
-  function getNumbers(){
-    return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
-  }
 
 function App() {
 
-  const [numbers,setNumbers] = useState([]);
-  const ref = useRef(null);
-
-  useEffect(()=>{
-    const nums = getNumbers();
-    setNumbers(nums);
-  }, [])
-
-  useLayoutEffect(()=>{
-    if(numbers.length === 0){
-      return;
-    }
-
-    for(let i = 0; i<300000000; i++){} //고의적 딜레이 
-
-    ref.current.scrollTop = ref.current.scrollHeight;
-  },[numbers])  //scroll이 자동으로 최하단에 위치하도록 조치
+  const users = [
+    {name: "김철수", age: "16"},
+    {name: "이영희", age: "24"},
+    {name: "김민수", age: "46"},
+    {name: "홍길동", age: "35"},
+    {name: "홍민영", age: "12"},
+    {name: "김영지", age: "15"},
+    {name: "윤동지", age: "100"},
+  ]
 
   return (
-    <>
-      <button onClick={()=> setNumbers( getNumbers())}>Reset</button>
-      <div 
-      ref={ref} 
-      style={
-      {
-        height: "300px",
-        border: "1px solid blue",
-        overflow: "scroll"
-      }}
-    >
-      {numbers.map((number,idx)=>(
-        <p key={idx}>{number}</p>
-      ))}
+     <div className="container">
+      <div className="search-container">
+        <input placeholder="여기다 검색하세요" />
+        <ul>
+          {users.map((user)=>(  
+             <li key={user.name}>
+            <span>{user.name}</span>
+            <span>{user.age}세</span>
+          </li>
+        ))}
+        </ul>
+      </div>
     </div>
-    </>
   );
 }
 
 export default App;
 
-//개발자 모드에서 Performance로 들어가 CPU를 6배 느리게 설정 해주자.
-//useEffect는 비동기적으로 실행되기 때문에 'scroll이 자동으로 최하단에 위치하도록 조치'
-//해주는 코드가 실행되기 전에 랜더링 한다. 때문에 깜빡이는 UI변화가 생기게 된다.
-//하지만 useLayoutEffect는 동기적 작동 방식이기 때문에 useLayoutEffect가 먼저 실행되고,
-//랜더링이 되기때문에 화면이 깜빡이지 않는다.
+//함수가 너무 많아(특히 서버비에 부담이 되는 함수) 문제가 될때 
+//Debounce와 Throttle을 사용한다. 이 기능은 함수가 너무 자주 호출되는것을 방지한다.
+//Debounce: 특정 이벤트가 연속적으로 발생할 때, 제일 마지막 이벤트가 발생한 후
+//일정 시간이 지난 후에 함수를 호출함
