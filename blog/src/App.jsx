9969ab326ad5a6c3665dd7cc889b460da06b8f10
ohwooje <dkwooje@naver.com>
2024-11-20 16:43:45 +0900

    Throttle 적용 전 2

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,49 +1,38 @@
-import { useEffect, useState } from "react";
-import { useDebounce } from "./debounce";
+import { useState } from "react";
+import "./App.css" 
 
-function fetchDataFromServer(value){
-  if(!value){
-    return [];
+function hackLottoNumbers(){
+  console.log("로또 번호 해킹중")
+  const lottoNumbers = [];
+  for( let i = 0; i<6; i++){
+    const number = Math.floor(Math.random() * 45) + 1;
+    lottoNumbers.push(number);
   }
-  console.log("서버로부터 데이터를 가져오는 중....")
-  const users = [
-    {name: "김철수", age: "16"},
-    {name: "이영희", age: "24"},
-    {name: "김민수", age: "46"},
-    {name: "홍길동", age: "35"},
-    {name: "홍민영", age: "12"},
-    {name: "김영지", age: "15"},
-    {name: "윤동지", age: "100"},
-  ]
-  return users.filter((user)=> user.name.startsWith(value));
+  return lottoNumbers;
 }
 
+
 function App() {
+  const [lottoNumbers,setLottoNumbers] = useState([0,0,0,0,0,0]);
+
+  function handleClick(){
+    const result = hackLottoNumbers();
+    setLottoNumbers(result)
+  }
+
   
-  const [input,setInput] = useState("");
-  const debouncedInput = useDebounce(input, 500);
-  const [result,setResult] = useState([]);
-
-  useEffect(()=>{ 
-    const users = fetchDataFromServer(debouncedInput);
-    setResult(users);
-  },[debouncedInput])
-
   return (
-     <div className="container">
-      <div className="search-container">
-        <input 
-        placeholder="여기다 검색하세요" 
-        value={input} 
-        onChange={(event) => setInput(event.target.value)}/>
-        <ul>
-          {result.map((user)=>(  
-             <li key={user.name}>
-            <span>{user.name}</span>
-            <span>{user.age}세</span>
-          </li>
-        ))}
-        </ul>
+    <div className="container">
+      <h1 className="title">로또 번호 맞춰줄게</h1>
+      <button className="button" onClick={handleClick}>
+        번호 맞추기
+      </button>
+      <div className="numbers">
+        {lottoNumbers.map((number, idx)=>(   
+            <span key={idx} className="number">
+              {number}
+             </span>
+          ))}
       </div>
     </div>
   );
@@ -51,3 +40,5 @@ function App() {
 
 export default App;
 
+//Throttle 함수가 한번 호출 되면, 일정 시간이 지나기 전에 다시 호출되지 않도록 막는 것.
+//
