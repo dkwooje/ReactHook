90c2db4d2dc8e1588fcd74640b4180a90276afe0
ohwooje <dkwooje@naver.com>
2024-11-20 15:20:14 +0900

    useLayoutEffect 사용

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect,useLayoutEffect, useState } from "react";
 
 function App() {
   const [count, setCount] =useState(0);
 
   useEffect(()=>{
     console.log("useEffect", count)
+  },[count])
+
+  useLayoutEffect(()=>{
+    console.log("useLayoutEffect", count)
   },[count])
 
 
@@ -21,7 +25,4 @@ function App() {
 }
 
 export default App;
-//useLayoutEffect는 useEffect와 구조가 비슷하다.
-//useEffect는 화면이 업데이트(랜더링)이 된 후 실행되지만
-//useLayoutEffect는 먼저 실행되고 랜더링이 된다.
-//useLayoutEffect는 UI변화를 더 정교하게 나타낼 수 있다.
+//useLayoutEffect는 useEffect보다 먼저 실행된다.
