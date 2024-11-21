c6875cb283a7383176d5fbc857ef5ce12c867c21
ohwooje <dkwooje@naver.com>
2024-11-21 14:54:34 +0900

    React.Fragment 기초2

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,10 +1,11 @@
 import Component from "./Component";
-
+import React from "react";
+import './App.css';
 function App() {
 
   
   return (
-    <div className="App" >
+    <div>
       <Component />
     </div>
   );
@@ -12,4 +13,5 @@ function App() {
 
 export default App;
 
-//React.Fragment
+//App.css에서 스타일링이 <div>로 감싸 엘리먼트에 적용하면 망가진다
+//하지만 <>를 사용하면 스타일링이 그대로 적용된다.
