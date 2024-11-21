b58397057bdc1b4f9c4197bc22a49dec46741667
ohwooje <dkwooje@naver.com>
2024-11-21 15:31:34 +0900

    List 와 Key 적용전

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,27 +1,32 @@
-
-import React from "react";
-import './App.css';
-import Column from "./Column";
-
-
+import { useState } from "react";
 
 function App() {
 
+  const [inputValue,setInputValue] = useState('');
+  const [list,setList] =useState(['밥먹기','코딩하기']);
+
+  function addToList(){
+    setList((prevList)=>{
+      return [inputValue, ...prevList];
+    });
+    setInputValue('');
+  }
+
   return (
-    <div>
-      <table>
-        <tbody>
-            <tr>
-              <Column />
-            </tr>
-            <tr>
-              <Column />
-            </tr>
-        </tbody>
-      </table> 
-    </div>
+    <>
+      <input value={inputValue}
+      onChange={(e)=> setInputValue(e.target.value)}
+      />
+      <button onClick={addToList}>추가</button>
+      <ul>
+        {list.map((item)=>{
+            return <li>{item}</li>
+        })}
+      </ul>
+    </>
   );
 }
 
 export default App;
-
+//list형태로 엘리먼트를  표현하면 key값을 prop으로 가져야한다.
+//그렇지 않으면 모든 list(새로생긴 list 포함)가 공유된다.
